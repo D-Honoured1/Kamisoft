49b32628ac17b53bbbd0185dccbdb607cca7453a
D-Honoured1 <jamesalvez347@gmail.com>
2025-09-18 13:25:57 +0100

    lol

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -12,12 +12,13 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Alert, AlertDescription } from "@/components/ui/alert"
 import { Eye, EyeOff, Shield } from "lucide-react"
-import { createBrowserClient } from "@/lib/supabase/client"
+//import { createBrowserClient } from "@/lib/supabase/client"
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
 import Link from "next/link"
 
 export default function AdminLogin() {
   const router = useRouter()
-  const supabase = createBrowserClient()
+  const supabase = createBrowserSupabaseClient()
   const [isLoading, setIsLoading] = useState(false)
   const [showPassword, setShowPassword] = useState(false)
   const [error, setError] = useState("")
